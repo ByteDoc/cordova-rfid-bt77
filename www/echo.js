f427f3ae205d487df459eddbd6ae86d3b9770ec2
wolfgangrabenstein <wolfgang.rabenstein@neptune-software.com>
2016-09-29 13:37:14 +0200

    Update echo.js

--- www/echo.js
+++ www/echo.js
@@ -6,6 +6,8 @@ module.exports = {
          * EinEcho has to match with <feature name="EinEcho"> from the file plugin.xml
          *
          * testeingabe has to match with action.equals("testeingabe") from the *.java-file
+         *
+         * name are the arguments (JSONArray) that will be sent to the *.java-file
          */
         cordova.exec(successCallback, errorCallback, "EinEcho", "testeingabe", [name]);
     }
