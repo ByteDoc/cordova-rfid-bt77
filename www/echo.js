cd68a38b4288affebc04f9d9a5087dae0b3c690a
wolfgangrabenstein <wolfgang.rabenstein@neptune-software.com>
2016-09-29 12:09:56 +0200

    Update echo.js

--- www/echo.js
+++ www/echo.js
@@ -2,6 +2,11 @@
 
 module.exports = {
     echo: function (name, successCallback, errorCallback) {
+        /**
+         * EinEcho has to match with <feature name="EinEcho"> from the file plugin.xml
+         *
+         * testeingabe has to match with action.equals("testeingabe") from the *.java-file
+         */
         cordova.exec(successCallback, errorCallback, "EinEcho", "testeingabe", [name]);
     }
 };
