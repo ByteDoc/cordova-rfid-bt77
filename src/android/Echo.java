54213b49870875c1374c7d1cdedb28e25e3b353c
wolfgangrabenstein <wolfgang.rabenstein@neptune-software.com>
2016-09-29 13:29:42 +0200

    Update Echo.java

--- src/android/Echo.java
+++ src/android/Echo.java
@@ -15,7 +15,7 @@ public class Echo extends CordovaPlugin {
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 		if (action.equals("testeingabe")) {
-			String message = args.getString(0)
+			String message = args.getString(0);
 			this.echo(message, callbackContext);
 			return true;
 		}
