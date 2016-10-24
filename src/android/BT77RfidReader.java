0fefafffbe1a65c27a9174f52977673933dbcb65
wolfgangrabenstein <wolfgang.rabenstein@neptune-software.com>
2016-10-24 12:16:57 +0200

    Adding import org.json.simple.*

--- src/android/BT77RfidReader.java
+++ src/android/BT77RfidReader.java
@@ -18,6 +18,7 @@ import com.sevenid.mobile.reader.api.parameters.WriteParameters;
 import com.sevenid.mobile.reader.bt77.RfidReader;
 
 import java.util.*;
+import org.json.simple.*
 
 /**
 * This class echoes a string called from JavaScript.
