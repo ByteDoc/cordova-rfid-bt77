bc17274266154a8b867f2ab349cb68819f1e77f8
wolfgangrabenstein <wolfgang.rabenstein@neptune-software.com>
2016-11-22 20:38:35 +0100

    One last try

--- src/android/BT77RfidReader.java
+++ src/android/BT77RfidReader.java
@@ -110,7 +110,53 @@ public class BT77RfidReader extends CordovaPlugin {
     
     private boolean startRFIDReader(){
         if(reader == null){
-            this.reader = new CustomRfidReader(cordova.getActivity());
+            this.reader = new RfidReader(cordova.getActivity()){
+				Field fReader = RfidReader.class.getDeclaredField("reader");
+				fReader.setAccessible(true);
+				uhfreader = fReader.get(this.reader);
+				
+				@Override
+				public InventoryResult getInventory(InventoryParameters param){
+					System.out.println("This is a test if this method will really be overwritten!!!");
+					InventoryResult result = new InventoryResult();
+					
+					HashMap<String, Epc> unfilteredInventory = new HashMap();
+					for (int i = 0; i < param.getCycleCount(); i++){
+						List<byte[]> currentInventory = uhfreader.inventoryRealTime();
+						if ((currentInventory != null) && (!currentInventory.isEmpty())) {
+							for (byte[] epc : currentInventory) {
+								if ((epc != null) && (epc.length > 0)){
+									String epcStr = Tools.Bytes2HexString(epc, epc.length);
+									if (survivesFilter(epcStr, param)){
+										Epc old = (Epc)unfilteredInventory.get(epcStr);
+										if (old == null) {
+											unfilteredInventory.put(epcStr, new Epc(epcStr));
+										} else {
+											old.incrementSeenCount();
+										}
+									}
+								}
+							}
+						}
+						try{
+							Thread.sleep(50L);
+						} catch (InterruptedException localInterruptedException1) {}
+					}
+					List<Epc> thresholdFilteredEpcList = new ArrayList();
+					for (Iterator i = unfilteredInventory.entrySet().iterator(); i.hasNext();){
+						Map.Entry currentEntry = (Map.Entry)i.next();
+						Epc currentEpc = (Epc)currentEntry.getValue();
+						if (currentEpc.getSeenCount() >= param.getCountThreshold()) {
+							thresholdFilteredEpcList.add(currentEpc);
+						}
+					}
+					Epc[] a = new Epc[thresholdFilteredEpcList.size()];
+					result.setInventory((Epc[])thresholdFilteredEpcList.toArray(a));
+					result.setOperationStatus(OperationStatus.STATUS_OK);
+					
+					return result;
+				
+			};
         }
         if(!this.reader.isBusy() || !this.reader.isOpen()){
             Log.i("BT77RfidReader", "startRFIDReader: this.reader.open(): " + this.reader.open());
@@ -418,7 +464,7 @@ public class BT77RfidReader extends CordovaPlugin {
     */
 }
 
-class CustomRfidReader extends RfidReader{
+/* class CustomRfidReader extends RfidReader{
 	Field field = RfidReader.class.getDeclaredField("reader");
 	field.setAccessible(true);
 	
@@ -463,4 +509,4 @@ class CustomRfidReader extends RfidReader{
 		
 		return result;
 	}
-}
+} */
